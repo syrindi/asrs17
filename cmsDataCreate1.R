41df75390fe578d3b3500c935e8224895fe233a6
syrindi <pjleemd@gmail.com>
2017-06-04 19:31:22 -0400

    prior to rerranging the code from beginning to the end - logic wise

--- cmsDataCreate1.R
+++ cmsDataCreate1.R
@@ -1,8 +1,9 @@
 cmsData = readRDS("~/Dropbox/cms/allData/enhancedData.rds")
 
 cmsData = data.table(cmsData)
-
+load("~/Dropbox/cms/eyeDataFinal.rda")
 unique(cmsData$year)  #2012, 2013, 2014
+
 
 # - select specified fields
 unique(cmsData$providertype)
@@ -11,3 +12,8 @@ eyes = cmsData[providertype ==field]
 
 
 # - add additional fields
+
+
+zip = read_csv("~/Dropbox/cms/zip_code_database.csv")
+
+
